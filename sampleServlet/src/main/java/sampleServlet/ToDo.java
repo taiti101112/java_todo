1ca820eeaafb1b14e310782596e30b14adfc62bf
taiti101112 <bbbstaiti@gmail.com>
2024-11-08 11:01:55 +0900

    テスト

--- sampleServlet/src/main/java/sampleServlet/ToDo.java
+++ sampleServlet/src/main/java/sampleServlet/ToDo.java
@@ -1,5 +1,44 @@
 package sampleServlet;
 
 public class ToDo {
+	private int id;
+	private String task;
+	private boolean completed;
+	
+	public ToDo() {
+		
+	}
+	
+	public ToDo(int id, String task) {
+		this.id = id;
+		this.task = task;
+		this.completed = false;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getTask() {
+		return task;
+	}
+	
+	public boolean getCompleted() {
+		return completed;
+	}
+	
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setTask(String task) {
+		this.task = task;
+	}
+	
+	public void setCompleted(boolean completed) {
+		this.completed = completed;
 
+	}
 }
+
